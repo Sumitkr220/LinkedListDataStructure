7fc8a0b77b82e08d0aeeef9f0b03ef14a505b3c4
sumitpandit <sumitpandit178@gmail.com>
2020-10-25 09:36:38 +0530

    [Sumit] Add: Creating LinkedList

---
+++ Linked-List/Linked-List/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Linked_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkList Data-Struture");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
